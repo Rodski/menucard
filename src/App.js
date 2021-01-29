a939e88f5c12a6917ce43480302793dbc813cd47
Rodrigue Stultiens <u0131108@thomasmore.be>
2021-01-29 14:01:22 +0100

    add initial files

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                {/*<p>*/}
+                {/*  Edit <code>src/App.js</code> and save to reload.*/}
+                {/*</p>*/}
+                {/*<a*/}
+                {/*  className="App-link"*/}
+                {/*  href="https://reactjs.org"*/}
+                {/*  target="_blank"*/}
+                {/*  rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*  Learn React*/}
+                {/*</a>*/}
+            </header>
+            <body>
+            <div className="root">
+                <div>
+                    <h1>Menu</h1>
+                    <h1>cola</h1>
+                    <h1>water</h1>
+                    <h1>bier</h1>
+                    <h1>wijn</h1>
+                </div>
+            </div>
+
+            </body>
+        </div>
+    );
 }
 
 export default App;
